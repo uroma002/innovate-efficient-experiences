f0301f9de541461c9eec41c2c77adb0e925e079f
Uroma002 <146116449+uroma002@users.noreply.github.com>
2023-11-22 17:41:15 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,51 +1,39 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-    }
-        }
-            System.out.println("Random Number: " + item);
-
-}
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-public class RandomDataGenerator {
-        int[] data = new int[10];
-
-        }
-    public static void main(String[] args) {if __name__ == "__main__":
-    for item in data:
-    main()
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
-
-def main():
-import random
-    return data
-    data = generate_random_data()
-def generate_random_data():def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-
-    main()
-    return data
-        print(f"Random Number: {item}")
-
-
 
 if __name__ == "__main__":
-def main():
+
+    main()
+import random
     for item in data:
-    data = generate_random_data()    public static void main(String[] args) {
+    return data
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
 
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-        }
-public class RandomDataGenerator {
+        print(f"Random Number: {item}")
+def main():
+    data = generate_random_data()
+
     }
-        for (int item : data) {
-        }
             data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        for (int item : data) {
 }
-
+public class RandomDataGenerator {
+        }
+        }
             System.out.println("Random Number: " + item);
+
+        int[] data = new int[10];if __name__ == "__main__":
+
+    main()
+
+
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+import random
+def generate_random_data():
+def main():
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
